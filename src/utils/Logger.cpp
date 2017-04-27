fd1de84fe8514ffccf2150c41cf126477367b492
David Watkins <djrival7@gmail.com>
2017-04-27 04:49:19 -0400

    Added Logger

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Logger.h"
+
+#define _LOG(level) \
+    std::cout << Tedm::Logger::level_strings[int(level)] << " " __FILE__  ":"  << __LINE__ << ": "
+
+void Tedm::Logger::log(Tedm::Logger::Level level, std::string msg) {
+    if(level > getLevel()) {
+        _LOG(level) << msg << std::endl;
+    }
+}
+
+Tedm::Logger::Level Tedm::Logger::getLevel() {
+    return level;
+}
+
+void Tedm::Logger::setLevel(Tedm::Logger::Level level) {
+    Tedm::Logger::level = level;
+}
+
+Tedm::Logger::Logger(Tedm::Logger::Level level) {
+    Tedm::Logger::level = level;
+}
+
+Tedm::Logger::~Logger() { }
--- src/utils/Logger.h
+++ src/utils/Logger.h
@@ -5,9 +5,39 @@
 #ifndef TEDM_LOGGER_H
 #define TEDM_LOGGER_H
 
+#include <iostream>
+
 namespace Tedm {
     class Logger {
+    public:
+        enum Level {
+            LOG_DEBUG = 1;
+            LOG_INFO = 2;
+            LOG_WARN = 3;
+            LOG_ERROR = 4;
+            LOG_NONE = 5;
+        };
 
+        const std::string level_strings[] = {
+                "DEBUG",
+                "INFO",
+                "WARN",
+                "ERROR",
+                "NONE"
+        };
+
+        Logger(Level level);
+
+        ~Logger();
+
+        void log(Level level, std::string msg);
+
+        static Level getLevel();
+
+        static void setLevel(Level level);
+
+    private:
+        static Level level;
     };
 }
 
