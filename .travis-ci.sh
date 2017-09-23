7f0490608ff26b191c805c4068c22c2d8950b677
David Watkins <djrival7@gmail.com>
2017-09-23 06:03:30 -0400

    Added changes to cmake to build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APT="libsdl2-2.0 libsdl2-dev libsdl2-image-dev cmake"
+APT="libsdl2-2.0 libsdl2-dev libsdl2-image-dev cmake libegl1-mesa-dev libgles2-mesa-dev"
 	 	 
 sudo apt clean
 sudo apt update -qq
