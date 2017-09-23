5b39acc9c13e20a42916abf028669e5bd7ef18ae
David Watkins <djrival7@gmail.com>
2017-09-23 06:00:57 -0400

    Added changes to cmake to build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,8 +2,9 @@
 
 APT="libsdl2-2.0 libsdl2-dev libsdl2-image-dev cmake"
 	 	 
-sudo apt-get update -qq
-sudo apt-get install -qq $APT
+sudo apt clean
+sudo apt update -qq
+sudo apt install -qq $APT
 mkdir build && cd build
 cmake ..
 make
